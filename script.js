bd0e644b31e59fff21c9d476777a3f712ac2c642
Miranda46 <80054292+Miranda46@users.noreply.github.com>
2024-11-01 16:19:01 -0500

    bugfixing delete

--- script.js
+++ script.js
@@ -16,30 +16,67 @@ function addBookToLibrary(book, library){
     if (book instanceof Book) library.push(book);
 }
 
+function bookDataHTML(elem) {
+    let index = myLibrary.length - 1;
+    const data = document.createElement('div');
+    data.appendChild(elem);
+    const button1 = document.createElement('button');
+    button1.className = 'book_button';
+    button1.addEventListener('click', function(){
+        
+        removeBook(index);
+    });
+    button1.innerText = 'delete';
+    data.appendChild(button1);
+
+    return data
+}
+
 function bookToHTML(book) {
     const elem = document.createElement('div')
     elem.className = 'book';
     const text = document.createElement('p');
     text.innerHTML = book.title;
     elem.appendChild(text);
-    return elem
+    
+    return bookDataHTML(elem);
 }
 
 function addBook(event) { 
     const form = document.querySelector('form');
-    var formData = new FormData(form);
+    const formData = new FormData(form);
     const title = formData.get('title');
     const author = formData.get('author');
     const numPages = formData.get('num_pages');
     const read = formData.get('read');
+
     let book = new Book(title, author, numPages, read);
+    console.log(myLibrary)
     addBookToLibrary(book, myLibrary);
-    let shelf = document.querySelector('.library');
-    let elem = bookToHTML(book);
-    shelf.appendChild(elem);
-    form.reset()
+
+    startLibrary();
+
+    form.reset();
 }
 
+function appendBookToLibrary(book) {
+    let elem = bookToHTML(book);
+    let library = document.querySelector('.library');
+    library.appendChild(elem);
+}
+
+function startLibrary() {
+    document.querySelector('.library').innerHTML = '';
+    for (book in myLibrary){
+        appendBookToLibrary(myLibrary[book]);
+    }
+}
+
+function removeBook(index) {
+    myLibrary.splice(index, 1);
+    startLibrary();
+    console.log('deon')
+}
 
 let add_button = document.getElementById('add_button');
 add_button.addEventListener('click', addBook);
