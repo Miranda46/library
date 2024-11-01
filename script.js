811192712a4a5f39cb5f195121dd9149884da202
Miranda46 <felipem2001@gmail.com>
2024-11-01 11:37:22 -0500

    scaffolding

--- script.js
+++ script.js
@@ -17,19 +17,38 @@ function addBookToLibrary(book, library){
 }
 
 function bookToHTML(book) {
-    let elem = document.createElement('div')
+    const elem = document.createElement('div')
     elem.className = 'book';
-    let text = document.createElement('p');
+    const text = document.createElement('p');
     text.innerHTML = book.title;
     elem.appendChild(text);
     return elem
 }
 
-function addBook(title, author, pageNum, read) { 
-    let book = new Book(title, author, pageNum, read);
+function addBook(event) { 
+    const form = document.querySelector('form');
+    var formData = new FormData(form);
+    const title = formData.get('title');
+    const author = formData.get('author');
+    const numPages = formData.get('num_pages');
+    const read = formData.get('read');
+    let book = new Book(title, author, numPages, read);
     addBookToLibrary(book, myLibrary);
-    let shelf = document.getElementsByClassName('shelf');
-    document.body.insertBefore(bookToHTML(book), shelf[0]);
+    let shelf = document.querySelector('.library');
+    let elem = bookToHTML(book);
+    shelf.appendChild(elem);
+    form.reset()
+}
 
+
+let add_button = document.getElementById('add_button');
+add_button.addEventListener('click', addBook);
+
+let formularButton = document.getElementById('formular_button');
+formularButton.addEventListener('click', showFormular);
+
+function showFormular(event) {
+    let formular = document.getElementById('form');
+    formular.style.visibility = 'visible';
 }
 
